105b66183344d53683868ae7eace51298d2a5f70
David Zhang <32676710+dzhang32@users.noreply.github.com>
2026-01-02 20:41:51 +0000

    test: avoid testing cli in GHA

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,8 +1,14 @@
+import os
+
+import pytest
 from click.testing import CliRunner
 
 from gust.cli import cli
 
 
+@pytest.mark.skipif(
+    os.getenv("GITHUB_ACTIONS") == "true", reason="Avoid emails from GHA."
+)
 def test_cli():
     runner = CliRunner()
     result = runner.invoke(
